3708a2b23ac9a9381aa819befeae383aa0f30047
MartinYankov <martin.d.yankov@gmail.com>
2024-04-09 23:05:18 +0300

    feat: add protected routes

---
+++ server-side/middleware/verifyToken.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Not Authenticated!" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) {
+      return res.status(403).json({ message: "Token is not valid!" });
+    }
+
+    //Attach userId to request from payload to check is owner of the post
+    req.userId = payload.id;
+
+    next();
+  });
+};
